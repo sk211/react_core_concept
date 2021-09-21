207ea8f45782d03120f2bead1d65a86d07233d60
sk211 <sujanmajumder211@gmail.com>
2021-09-21 11:37:32 +0600

    my 1ist react componnet

---
+++ src/App.js
@@ -0,0 +1,78 @@
+// import logo from './logo.svg';
+import "./App.css";
+
+function App() {
+  return (
+    <div>Hello world</div>
+  );
+}
+
+
+export default App;
+
+
+
+
+// {/* <Friend
+// name="Sujan majumder"
+// age="26"
+// job=" Web Development"
+// marrid="Single"
+// ></Friend>
+// <Friend name="akbor" age="26" job="TMSS" marrid="singel "></Friend>
+// <Friend
+// name="Ayon Majumder"
+// age="26"
+// job="Italy"
+// marrid="singel "
+// ></Friend>
+// <Friend
+// name="Anik Majumder"
+// age="26"
+// job="Garmance "
+// marrid="singel "
+// ></Friend> */}
+
+{/* <div style={{ display: "flex", justifyContent: "space-around" }}>
+      <ul>
+        {
+          nayoks.map(nayok => <li>{nayok}</li>)
+        } 
+      </ul>
+
+{
+  cinemas.map(cinama =><div>
+    <li>{cinama.nayok}</li>
+    <li>{cinama.nayika}</li>
+    </div>
+    )
+}   
+    </div> */}
+  // const nayoks = ["sujan", "akbor", "anik", "tapos", "omar sani"]
+  // const  cinemas =[
+
+  //   {nayok: "kubar", nayika: "kopila"},
+  //   {nayok: "Rubel", nayika: "Moushumi"},
+  //   {nayok: "Razzak", nayika: "moshumi"},
+  // ]
+
+// function Friend(props){
+//   return(
+//     <div>
+//      <p>{props.name}</p>
+//      <p>{props.age}</p>
+//      <p>{props.job}</p>
+//      <p>{props.marrid}</p>
+//     </div>
+//   )
+// }
+
+// function Cinema(props){
+//   return(
+//     <div>
+//       <h2>Nayok: {props.nayok}</h2>
+//       <h2>Nayok: {props.nayika}</h2>
+//     </div>
+//   )
+// }
+
